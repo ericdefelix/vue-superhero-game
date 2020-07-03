df92964328d68c5c4ee09fb2e113dd9b6ca2eaa2
Eric De Felix <ericdefelix@gmail.com>
2020-07-04 03:10:20 +1000

    first commit

---
+++ layouts/split-column.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This is a split column layout
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    layout: 'split-column'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
